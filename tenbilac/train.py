gradients are very different there must be a mistake somewhere

tenbilac/train.py
@@ -566,8 +566,9 @@ class Training:
                 tmpnet.layers[li].weights -= eta * np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2))) #Best take at vectorization so far.. Note that numpy's tensordot function doesn't work with masked array
                 tmpnet.layers[li].biases -= eta * np.sum(deltas[li],(0,2))
                 
-                logger.info("GRADIENT IN LAYER {} IS \n{}".format(li+1,np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2)))))
-                logger.info("NUMERICAL GRADIENT IN LAYER {} IS \n{}".format(li+1,self.numgrad(li)))
+                #logger.info("GRADIENT IN LAYER {} IS \n{}".format(li+1,np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2)))))
+                #logger.info("NUMERICAL GRADIENT IN LAYER {} IS \n{}".format(li+1,self.numgrad(li,epsilon = 0.00000001)))
+                logger.info("\n{}".format(np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2)))-self.numgrad(li,epsilon = 0.00000000001)))
 
                 self.net = tmpnet
                 	
@@ -604,7 +605,7 @@ class Training:
         self.end()        
         
 
-	def numgrad(self,li,epsilon = 0.0001):
+    def numgrad(self,li,epsilon = 0.0001):
 		
 		tmpnet = self.net
 		grad = np.ones(np.shape(tmpnet.layers[li].weights))
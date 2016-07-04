7053faaf336ca5440a2391cbd2fddfa4edb72eb4
Riccardo de Lutio <riccardo.delutio@gmail.com>
2016-07-04 10:42:36 +0200

    adding some mods that where made in checkinggradient

--- tenbilac/train.py
+++ tenbilac/train.py
@@ -555,11 +555,12 @@ class Training:
                 deltas[i] = self.net.derivative_run(self.dat.traininputs,len(self.net.layers)+i) * np.rollaxis(np.dot(np.rollaxis(self.net.layers[i+1].weights,1),deltas[i+1]),1)
 
             for li in range(len(self.net.layers)):
-                self.net.layers[li].weights -= eta * np.tensordot(deltas[li],tmpnet.par_run(self.dat.traininputs,li),((0,2),(0,2))) #Best take at vectorization so far.. Note that numpy's tensordot function doesn't work with masked array
-                self.net.layers[li].biases -= eta * np.sum(deltas[li],(0,2))
-            
-            tmpnet = self.net
+                tmpnet.layers[li].weights -= eta * np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2))) #Best take at vectorization so far.. Note that numpy's tensordot function doesn't work with masked array
+                tmpnet.layers[li].biases -= eta * np.sum(deltas[li],(0,2))
+                
+                #logger.info("\n{}".format(np.tensordot(deltas[li],self.net.par_run(self.dat.traininputs,li),((0,2),(0,2)))-self.numgrad(li,epsilon = 0.0000001)))
 
+                self.net = tmpnet
                 	
        
 
